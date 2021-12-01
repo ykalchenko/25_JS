6c3a747c619020f5c820a2033e7ee649f51dd868
ykalchenko <yuliia.kalchenko@gmail.com>
2021-12-02 01:24:35 +0200

    add 3htmls

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+alert("Hello world!");
+let userName = "Yuliia Kalchenko";
+let name = "Yuliia Kalchenko";
+let $name = "Yuliia Kalchenko";
+let _userName = "Yuliia Kalchenko";
+let NAME = "Yuliia Kalchenko";
+
+// let user-Name = "Yuliia Kalchenko"; 
+/* let 1name = "Yuliia Kalchenko";
+ let ім'я = "Yuliia Kalchenko";
+ let let = "Yuliia Kalchenko"; */
+
+  let message;
+message = 'Hello world!';
+  
+let massege = "Hello world!";
+
+let massege = "Hello", massege2 = "world", massege3 = "!";
+
+let massege = "Hello";
+let massege2 = "world";
+let massege3 = "!";
+
+let massege = "Hello",
+  massege2 = "world",
+  massege3 = "!";
+
+let massege = "Hello"
+  , massege2 = "world"
+  , massege3 = "!";
